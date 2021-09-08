4c7dc6771f79e8bd25e2db2ae39c3823ab2ff9fa
Chris Taylor <chris@cronofy.com>
2021-09-08 20:52:16 +0100

    Initial files

---
+++ backend.tf
@@ -0,0 +1,13 @@
+# The block below configures Terraform to use the 'remote' backend with Terraform Cloud.
+# For more information, see https://www.terraform.io/docs/backends/types/remote.html
+terraform {
+  backend "remote" {
+    organization = "website"
+
+    workspaces {
+      name = "terraform"
+    }
+  }
+
+  required_version = "1.0.5"
+}
